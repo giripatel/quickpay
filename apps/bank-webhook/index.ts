774a25d430dc804a77b5038da7d187c6e1c94bea
Giridhar <giridharpatel143@gmail.com>
2024-04-09 23:30:24 +0530

    Added bank-webhook server and it's basic login

---
+++ apps/bank-webhook/index.ts
@@ -0,0 +1,41 @@
+import express from 'express'
+import db from '@quickpay/db/client'
+
+const app = express();
+
+app.post('/hdfcWebhook',async (req,res) => {
+
+    const paymentInformation = {
+        // token is to identify the on ram transaction of provider
+        token : req.body.token,
+        userId: req.body.user_identifier,
+        amount: req.body.amount
+    }
+
+    // TODO : add transaction logic
+    await db.balance.update({
+        where : {
+            id: req.body.user_identifier,
+        },
+        data : {
+           amount : {
+            increment : paymentInformation.amount
+           }
+        }
+    })
+
+    await db.onRampTransaction.update({
+        where : {
+            token : paymentInformation.token
+        },
+        data : {
+            status : "Success"
+        }
+    })
+
+    return res.status(200).json({
+        message : "capurted"
+    })
+})
+
+app.listen(3000)
